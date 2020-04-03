bc5f881e00d9a0bb8ad67d1c1f6c4b848aae47dd
bdcolosi <bdcolosi27@gmail.com>
2020-04-03 12:55:54 -0400

    deleted items in app.js

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ cookieParser = require('cookie-parser'),
 logger = require('morgan');
 
 const indexRouter = require('./routes/index'),
- usersRouter = require('./routes/users');
+ imagesRouter = require('./routes/images'),
+ commentsRouter = require('./routes/comments');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +17,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/images', imagesRouter);
+app.use('/comments', commentsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
